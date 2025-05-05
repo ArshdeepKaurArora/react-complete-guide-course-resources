50c4f8547ceca3d10c8c2bae1eafa39265d6228b
Arsh <kaurarshdeep0103@gmail.com>
2025-05-05 18:55:16 +0530

    Unzip project file and use useContext hook to resolve props drilling issue'

---
+++ attachments/10 Advanced State Management with Context useReducer/01-starting-project/01-starting-project/src/App.jsx
@@ -0,0 +1,17 @@
+import {CartContext, CartProvider} from './store/shopping-cart-context.jsx';
+
+import Header from './components/Header.jsx';
+import Shop from './components/Shop.jsx';
+
+function App() {
+  return (
+    <>
+      <CartProvider>
+        <Header/>
+        <Shop />
+      </CartProvider>
+    </>
+  );
+}
+
+export default App;
