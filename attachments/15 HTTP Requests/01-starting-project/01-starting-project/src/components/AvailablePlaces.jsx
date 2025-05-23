4921a330392deccfbedab7b37425cf3bc27476fe
ArshdeepKaurArora <kaurarshdeep0103@gmail.com>
2025-05-23 15:19:30 +0530

    Sort places by user location and add error handling

--- attachments/15 HTTP Requests/01-starting-project/01-starting-project/src/components/AvailablePlaces.jsx
+++ attachments/15 HTTP Requests/01-starting-project/01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,21 +1,48 @@
 import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
+import Error from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
 
   const [places, setPlaces] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [fetchError, setFetchError] = useState(null);
 
   useEffect(() => {
     const fetchPlaces = async() => {
       setIsLoading(true);
-      const response = await fetch('http://localhost:3000/places');
-      const data = await response.json();
-      setPlaces(data.places);
-      setIsLoading(false);
+      try {
+        const response = await fetch('http://localhost:3000/places');
+
+        if (!response.ok) {
+          throw new Error("Unable to fetch the data.")
+        }
+
+        const data = await response.json();
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const { latitude, longitude } = position.coords;
+          setPlaces(sortPlacesByDistance(data.places, latitude, longitude));
+          setIsLoading(false);
+        });
+      } catch (error) {
+        setFetchError({message: error.message || "Unable to fetch the data."});
+        setIsLoading(false);
+      }
     }
     fetchPlaces();
   }, [])
+
+  if (fetchError) {
+    return (
+      <Error
+        title="Error"
+        message={fetchError.message}
+        onConfirm={() => setFetchError(null)}
+      />
+    );
+  }
 
   return (
     <Places
