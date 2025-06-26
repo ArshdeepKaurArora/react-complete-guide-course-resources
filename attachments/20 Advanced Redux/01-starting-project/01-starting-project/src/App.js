2b04591cb307eaeedf1bf39f51d5f125a6094b22
Arsh <kaurarshdeep0103@gmail.com>
2025-06-26 23:18:21 +0530

    add useeffect and action creator thunk to run async fetch call along with redux

--- attachments/20 Advanced Redux/01-starting-project/01-starting-project/src/App.js
+++ attachments/20 Advanced Redux/01-starting-project/01-starting-project/src/App.js
@@ -1,16 +1,36 @@
+import { Fragment, useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import Notification from './components/UI/Notification';
+import { useDispatch, useSelector } from 'react-redux';
+import { useRef } from 'react';
+import { sendCartData } from './store/cart';
 
 function App() {
+  const cart = useSelector(state => state.cart);
   const toggleCart = useSelector(state => state.cart.toggleCart);
-  
+  const notification = useSelector(state => state.ui.notification);
+  const dispatch = useDispatch();
+
+  const isInitial = useRef(true);
+
+  useEffect(() => {
+    if (isInitial.current) {
+      isInitial.current = false;
+      return;
+    }
+    dispatch(sendCartData(cart));
+  }, [cart, dispatch])
+
   return (
-    <Layout>
-      { toggleCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && <Notification status={notification.status} title={notification.title} message={notification.message}/>}
+      <Layout>
+        { toggleCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- attachments/20 Advanced Redux/01-starting-project/01-starting-project/src/store/cart.js
+++ attachments/20 Advanced Redux/01-starting-project/01-starting-project/src/store/cart.js
@@ -1,51 +1,94 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui";
 
 const initialCartState = {
-    items: [],
-    totalQuantity: 0,
-    totalAmount: 0,
-    toggleCart: false,
-}
+  items: [],
+  totalQuantity: 0,
+  totalAmount: 0,
+  toggleCart: false,
+};
 
 const cartSlice = createSlice({
-    name: 'cart',
-    initialState: initialCartState,
-    reducers: {
-        addItemToCart(state, action) {
-            const newItem = action.payload;
-            const existingItem = state.items.find(item => item.id === newItem.id);
-            state.totalQuantity++;
-            state.totalAmount += newItem.price;
-            if (!existingItem) {
-                state.items.push({
-                    id: newItem.id,
-                    title: newItem.title,
-                    price: newItem.price,
-                    quantity: 1,
-                    totalPrice: newItem.price,
-                })
-            } else {
-                existingItem.quantity++;
-                existingItem.totalPrice += newItem.price;
-            }
-        },
-        removeItemFromCart(state, action) {
-            const itemId = action.payload;
-            const existingItem = state.items.find(item => item.id === itemId);
-            state.totalQuantity--;
-            state.totalAmount -= existingItem.price;
-            if (existingItem.quantity === 1) {
-                state.items = state.items.filter(item => item.id !== itemId);
-            } else {
-                existingItem.quantity--;
-                existingItem.totalPrice -= existingItem.price;
-            }
-        },
-        toggleCart(state) {
-            state.toggleCart = !state.toggleCart;
+  name: "cart",
+  initialState: initialCartState,
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+      state.totalAmount += newItem.price;
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          title: newItem.title,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice += newItem.price;
+      }
+    },
+    removeItemFromCart(state, action) {
+      const itemId = action.payload;
+      const existingItem = state.items.find((item) => item.id === itemId);
+      state.totalQuantity--;
+      state.totalAmount -= existingItem.price;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== itemId);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice -= existingItem.price;
+      }
+    },
+    toggleCart(state) {
+      state.toggleCart = !state.toggleCart;
+    },
+  },
+});
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://backend-react-maximilian-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
         }
+      );
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+    };
+    try {
+      await sendRequest()
+        dispatch(
+          uiActions.showNotification({
+            status: "success",
+            title: "Success!",
+            message: "Sent cart data successfully!",
+          })
+        );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: error.message || "Sending cart data failed!",
+        })
+      );
     }
-})
+  };
+};
 
 export const cartActions = cartSlice.actions;
 
